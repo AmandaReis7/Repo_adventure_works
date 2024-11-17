b23a8c1fad40ff89e349d3abc82d6865e15eb40a
Amanda Reis <amanda.reis@unimedbh.com.br>
2024-11-17 23:11:11 +0000

    Finalizado a camada intermediária de produtos.

--- models/staging/stg_adw__orderdetail.sql
+++ models/staging/stg_adw__orderdetail.sql
@@ -1,16 +1,16 @@
 with 
     order_detail as (
         select  
-         salesorderdetailid as id_detalhe_pedido,  
-         salesorderid as id_pedido,  
-         carriertrackingnumber as num_rastreamento, 
-         orderqty as qtd_pedido,  
-         productid as id produto,  
-         specialofferid as oferta_especial,  
-         unitprice as preco_unitario,  
-         unitpricediscount as desc_preco_unit , 
-         rowguid, 
-         cast(modifieddate as date) data_modificacao
+         salesorderdetailid as id_detalhe_pedido,
+         salesorderid as id_pedido,
+         carriertrackingnumber as num_rastreamento,
+         orderqty as qtd_pedido,
+         productid as id_produto,
+         specialofferid as oferta_especial,
+         unitprice as preco_unitario,
+         unitpricediscount as desc_preco_unit,
+         rowguid,
+         cast(modifieddate as date) as data_modificacao
     from {{ source("projeto_adw", "SALESORDERDETAIL") }}
 
 )
