482c823c9184c8d07086fc235d3c7d099632191d
Amanda Reis <amanda.reis@unimedbh.com.br>
2024-11-26 12:57:06 +0000

    Finalizado a dim_cartao

---
+++ models/staging/stg_adw__creditcard.sql
@@ -0,0 +1,14 @@
+with
+    cartao_clientes as (
+        select
+         creditcardid,
+         cardtype,
+         cardnumber,
+         expmonth,
+         expyear,
+         cast(modifieddate as date) as data_modificacao
+        from {{source("projeto_adw", "CREDITCARD")}}
+
+    )
+    select * from
+    cartao_clientes
