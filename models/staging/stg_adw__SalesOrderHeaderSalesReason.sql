bad8025066ac15e59e614e15c42b921ed6cc1293
Amanda Reis <amanda.reis@unimedbh.com.br>
2024-11-26 19:52:40 +0000

    finalizado a dim_motivo_vendas

---
+++ models/staging/stg_adw__SalesOrderHeaderSalesReason.sql
@@ -0,0 +1,11 @@
+ with
+    pedidos_motivos_vendas as (
+        select
+         salesorderid as id_pedido_venda,
+         salesreasonid as id_motivo_venda,
+         cast(modifieddate as date) as data_modificacao
+        from {{source("projeto_adw", "SALESORDERHEADERSALESREASON")}}
+
+    )
+    select * from
+    pedidos_motivos_vendas
