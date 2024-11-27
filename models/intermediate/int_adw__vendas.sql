76bb3f850a7899591a114c7bc6b8c4bd349c52a8
Amanda Reis <amanda.reis@unimedbh.com.br>
2024-11-27 14:49:20 +0000

    finalizado a fato vendas

--- models/intermediate/int_adw__vendas.sql
+++ models/intermediate/int_adw__vendas.sql
@@ -1,4 +1,4 @@
- with
+with
     pedidos_detalhe as (
      select * 
      from {{ref("stg_adw__orderdetail")}}
@@ -6,7 +6,7 @@
     pedidos as (
      select *
      from {{ref('stg_adw__orderheader')}}
-),
+ ),
     pedidos_completa as (
      select
          pedidos.ID_CLIENTE,
@@ -23,7 +23,7 @@
          pedidos.DATA_ENVIO,
          pedidos.STATUS,
          pedidos.FLAG_PEDIDO_ONLINE,
-         pedidos.NUM_PEDIDO_COMPRA as pedido_compra,
+         pedidos.NUM_PEDIDO_COMPRA,
          pedidos.NUM_CONTA,
          pedidos.ID_ENDERECO_COBRANCA,
          pedidos.ID_ENDERECO_ENVIO,
@@ -34,22 +34,57 @@
          pedidos.SUBTOTAL,
          pedidos.VLR_IMPOSTO,
          pedidos.FRETE,
-        -- SUM(pedidos.FRETE) / COUNT(DISTINCT pedidos.NUM_PEDIDO_COMPRA) AS frete_rateado,
          pedidos.TOTAL_DEVIDO,
-         pedidos.COMENTARIO
+         pedidos.COMENTARIO,
+         pedidos.data_modificacao
 
      from pedidos_detalhe
      inner join pedidos on pedidos.ID_PEDIDO_VENDA = pedidos_detalhe.id_pedido
-     ),
+),
     metricas as (
         select
              pedidos_completa.*,
              pedidos_detalhe.QTD_PEDIDO * pedidos_detalhe.PRECO_UNITARIO AS valor_bruto,
              pedidos_detalhe.QTD_PEDIDO * pedidos_detalhe.PRECO_UNITARIO * (1 - pedidos_detalhe.DESC_PRECO_UNIT) AS valor_liquido,
-             -- Cálculo do ticket médio, com a subtração do valor bruto pelo valor de desconto
-             (pedidos_detalhe.QTD_PEDIDO * pedidos_detalhe.PRECO_UNITARIO  - pedidos_detalhe.QTD_PEDIDO * pedidos_detalhe.PRECO_UNITARIO * pedidos_detalhe.DESC_PRECO_UNIT) / COUNT(DISTINCT pedidos.NUM_PEDIDO_COMPRA) OVER(PARTITION BY pedidos.id_cliente) as numero_pedidos_cliente
+             valor_liquido/pedidos_completa.QTD_PEDIDO as ticket_medio
+                          
         FROM pedidos_completa
         INNER JOIN pedidos_detalhe ON pedidos_completa.id_pedido = pedidos_detalhe.id_pedido
+ ),
+    final_select as (
+        select
+             ID_CLIENTE,
+             ID_VENDEDOR,
+             ID_DETALHE_PEDIDO,
+             ID_PEDIDO,
+             ID_PRODUTO,
+             NUM_PEDIDO_COMPRA,
+             QTD_PEDIDO,
+             OFERTA_ESPECIAL as ID_OFERTA,
+             DATA_ENVIO,
+             DATA_VENCIMENTO,
+             cast(PRECO_UNITARIO as numeric(18,2)) as PRECO_UNITARIO,
+             cast(DESC_PRECO_UNIT as numeric(18,2)) as DESC_PRECO_UNIT,
+             cast(SUBTOTAL as numeric(18,2)) as SUBTOTAL,
+             cast(VLR_IMPOSTO as numeric(18,2)) as VLR_IMPOSTO,
+             cast(FRETE as numeric(18,2)) as FRETE,
+             cast(TOTAL_DEVIDO as numeric(18,2)) as TOTAL_DEVIDO,
+             cast(VALOR_BRUTO as numeric(18,2)) as VALOR_BRUTO,
+             cast(VALOR_LIQUIDO as numeric(18,2)) as VALOR_LIQUIDO,             
+             cast(TICKET_MEDIO as numeric(18,2)) TICKET_MEDIO,
+             STATUS,
+             ID_TAXA_CAMBIO,
+             ID_METODO_ENVIO,
+             ID_ENDERECO_ENVIO,
+             NUM_RASTREAMENTO,
+             NUM_CONTA,
+             ID_ENDERECO_COBRANCA,
+             ID_CARTAO_CRED,
+             CD_APROV_CARTAO_CRED,
+             FLAG_PEDIDO_ONLINE,
+             COMENTARIO,
+             DATA_MODIFICACAO
+        from metricas
     )
-SELECT * 
-FROM metricas
+     select * 
+     from final_select
---
+++ models/marts/fct_vendas__adw.sql
@@ -0,0 +1,43 @@
+with
+    fct_vendas as (
+     select *
+     from {{ref('int_adw__vendas')}}
+     
+    ),    
+    dim_cartao_clientes as (
+     select *
+     from {{ref('dim_cartao_clientes__adw')}}
+     
+    ),
+    dim_clientes as (
+     select *
+     from {{ref('dim_clientes__adw')}}
+     
+    ),
+    dim_endereco as (
+     select *
+     from {{ref('dim_endereco__adw')}}
+     
+    ),
+    dim_funcionarios as (
+     select *
+     from {{ref('dim_funcionarios__adw')}}
+     
+    ),
+    dim_motivos_vendas as (
+     select *
+     from {{ref('dim_motivos_vendas__adw')}}
+     
+    ),
+    dim_produtos as (
+     select *
+     from {{ref('dim_produtos__adw')}}
+     
+    )
+     
+ select * 
+ from fct_vendas
+
+
+
+
